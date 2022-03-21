813a0416c54787895737b14db4fcb36c7a663333
georgi Kostov <g3rg1.kostov@gmail.com>
2022-03-21 15:02:32 +0200

    Started tasks for 12. Exeption Handling

---
+++ Number Exeption/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Number_Exeption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var n = int.Parse(Console.ReadLine());
+                var result = Math.Sqrt(n);
+                if (double.IsNaN(result))
+                {
+                    throw new NotFiniteNumberException(result);
+                }
+
+                Console.WriteLine(result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number.");
+
+            }
+            catch(NotFiniteNumberException)
+            {
+                Console.WriteLine("Invalid Number.");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
