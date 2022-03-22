890092b26fe35c94de23bd348bb7f7431b859d10
georgi Kostov <g3rg1.kostov@gmail.com>
2022-03-22 18:56:41 +0200

    Continued work on Read BIN File

--- Read BIN File/Program.cs
+++ Read BIN File/Program.cs
@@ -7,9 +7,14 @@ namespace Read_BIN_File
     {
         static void Main(string[] args)
         {
-            string fileDir = @"SavedGame.bin";
+            string fileDir = @"C:\Users\Bankera\Source\Repos\g3rg1\CSharp-Principles-Problems\Read BIN File\SavedGame.bin";
+            string fileToWriteTo = @"C:\Users\Bankera\Source\Repos\g3rg1\CSharp-Principles-Problems\Read BIN File\FileToWriteTo.bin";
             var bytes = File.ReadAllBytes(fileDir);
-
+            var file = File.Open(fileToWriteTo, FileMode.Open);
+            file.Write(bytes, 0, bytes.Length);
+            file.Close();
+            file.Dispose();
+            Console.WriteLine("Done.");
         }
     }
 }
