5c80bc9c5da92009e62e7f666b6279f0c4667106
georgi Kostov <g3rg1.kostov@gmail.com>
2022-03-23 09:00:31 +0200

    Started Parse From File for 12. Exeption Handling.

---
+++ Parse From File/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Parse_From_File
+{
+    class Program
+    {
+        static double[] ParseFromFile(string path)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            var path = @"FileToParse.txt";
+
+        }
+    }
+}
