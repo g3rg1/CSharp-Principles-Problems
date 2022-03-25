b8bb3b5f0b2fb076ad04d261e96b09d1456cdb19
georgi Kostov <g3rg1.kostov@gmail.com>
2022-03-25 15:23:42 +0200

    Continued on Parse From File for 12. Exeption Handling.

--- Parse From File/Program.cs
+++ Parse From File/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Parse_From_File
 {
@@ -6,6 +8,28 @@ namespace Parse_From_File
     {
         static double[] ParseFromFile(string path)
         {
+            var nums = new List<double>();
+
+            try
+            {
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    while (reader.ReadLine() != null)
+                    {
+                        var line = reader.ReadLine();
+                        var strings = line.Split(' ');
+                        foreach (var num in strings)
+                        {
+                            nums.Add(double.Parse(num));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
         static void Main(string[] args)
