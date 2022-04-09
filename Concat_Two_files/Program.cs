4d94495678ce5ce5033d57b20d369d20000c0976
georgi Kostov <g3rg1.kostov@gmail.com>
2022-04-09 12:47:32 +0300

    Solved two tasks for 10. Text Files.

---
+++ Concat_Two_files/Program.cs
@@ -0,0 +1,60 @@
+﻿using LoremNET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Concat_Two_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePaths = new string[] {@"FirstFile.txt", @"SecondFile.txt"};
+            var exitFile = @"ExitFile.txt";
+
+            WriteFiles(filePaths);
+            
+            using(var fileWrite = new StreamWriter(exitFile))
+            {
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    using (var fileread = new StreamReader(filePaths[i]))
+                    {
+                        string line;
+
+                        while ((line = fileread.ReadLine()) != null)
+                        {
+                            fileWrite.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            var textExitFile = File.ReadAllText(exitFile);
+            Console.WriteLine(textExitFile);
+        }
+        static void WriteFiles(string[] filePaths)
+        {
+            var rng = new Random();
+
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                using (var writer = new StreamWriter(filePaths[i]))
+                {
+                    var textLines = new List<string>();
+
+                    for (int j = 0; j < rng.Next(2, 4); j++)
+                    {
+                        textLines.Add(Lorem.Sentence(2, 6));
+                    }
+
+                    textLines.Add($"End of FIle[{i + 1}].");
+
+                    for (int j = 0; j < textLines.Count; j++)
+                    {
+                        writer.WriteLine(textLines[j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TextFile_Read_Odd_Rows/Program.cs
@@ -0,0 +1,36 @@
+﻿using LoremNET;
+using System;
+using System.IO;
+
+namespace TextFile_Read_Odd_Rows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = @"FileToReadFrom.txt";
+            
+            //using(var fileWrite = new StreamWriter(fileName))
+            //{
+            //    for (int i = 0; i < 42; i++)
+            //    {
+            //        fileWrite.WriteLine($"[{i}] {Lorem.Sentence(5, 10)}");
+            //    }
+            //}
+
+            using (var fileRead = new StreamReader(fileName))
+            {
+                string line;
+                var lineIndex = 0;
+                while((line = fileRead.ReadLine()) != null)
+                {
+                    if(lineIndex%2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    lineIndex++;
+                }
+            }
+        }
+    }
+}
